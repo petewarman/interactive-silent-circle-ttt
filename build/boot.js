3b89581d13d0a6384d18918afacba29cefbca9f4
taseenb <esteban.almiron@gmail.com>
2015-09-11 10:58:57 +0100

    fixes on paths in boot.js

--- build/boot.js
+++ build/boot.js
@@ -23,10 +23,10 @@ define( [], function () {
   return {
     boot: function ( el, context, config, mediator ) {
       // Load CSS
-      addCSS( assetsPath + '/css/main.css' );
+      addCSS( assetsPath + 'css/main.css' );
 
       // Load main application
-      require( [ assetsPath + '/js/main.js'], function ( req ) {
+      require( [ assetsPath + 'js/main.js'], function ( req ) {
         // Main app returns a almond instance of require to avoid
         // R2 / NGW inconsistencies.
         req( ['main'], function ( main ) {
